first test and bugfix. Most code is working, but proper result is still missing. Error calculation seems to be incorrect

celestials.py
@@ -16,14 +16,16 @@ class CelestialBody:
         self.mass = mass
         self.kind = kind
         self.parent = parent
-        self.a = a           # semi-major axis
+        self.a = a * 1.496 * 1e8           # semi-major axis in km
 
-    def get_barycentric(self, t):
-        pos = get_body_barycentric(self.name, t).to_value      # cartesian representation in km
+    def get_barycentric(self, t, t0=2451545., tformat='jd'):
+        t_ = Time(t+t0, format=tformat)
+        pos = get_body_barycentric(self.name, t_).xyz.to('km').to_value()      # cartesian representation in au
         return pos
 
-    def get_barycentric_vel(self, t):
-        vel = get_body_barycentric_posvel(self.name, t).to_value
+    def get_barycentric_vel(self, t, t0=2451545, tformat='jd'):
+        t_ = Time(t + t0, format=tformat)
+        vel = get_body_barycentric_posvel(self.name, t_)[1].xyz.to('km/s').to_value()
         return vel
 
     def sphere_of_influence(self):
@@ -46,7 +48,7 @@ class CelestialGroup:
         self.parents = np.empty((0, ))
         self.kinds = np.empty((0, ))
         for arg in args:
-            if type(arg) is not type(CelestialBody):
+            if type(arg) is not CelestialBody:
                 raise TypeError('CelestialGroup __init__. Type of argument is not CelestialBody.')
             self.objects = np.append(self.objects, arg)
             self.names = np.append(self.names, arg.name)
@@ -55,13 +57,13 @@ class CelestialGroup:
             self.kinds = np.append(self.kinds, arg.kind)
         self.n = len(self.names)
 
-    def ephemeris(self, t):
+    def ephemeris(self, t, t0=2451545, tformat='jd'):
         pos = np.empty((3, self.n))
         for i in range(0, self.n):
-            pos[i] = self.objects[i].get_barycentric(t)
+            pos[:, i] = self.objects[i].get_barycentric(t, t0, tformat)
         return pos
 
     def get_body(self, name):
-        return self.objects[np.where(self.names == name)]
+        return self.objects[np.where(self.names == name)][0]
 
 

ode.py
@@ -15,10 +15,11 @@ def equations(t, r, objects):
     :return: vector dr with dx,dy,dz,ddx,ddy,ddz
     """
     dr = np.empty(r.shape)
-    dr[0:2] = r[3:]
+    dr[0:3] = r[3:6]
     obj_pos = objects.ephemeris(t)
+
     positions = np.empty((3, obj_pos[0, :].size+1))
-    positions[:, 0] = r[0:2]
+    positions[:, 0] = r[0:3]
     positions[:, 1:] = obj_pos
     dr[3:] = acceleration(positions, objects.mass)
     return dr
@@ -32,9 +33,8 @@ def acceleration(positions, mass):
     :param mass: masses of objects. mass[0] is sun, then other objects. numpy 2d array size (1, nobjects)
     :return: returns resulting acceleration a at point r[:, 0]. Should be ndarray with size (3, 1)
     """
-    rij = positions[:, 1:] - positions[:, 0]
+    rij = positions[:, 1:] - positions[:, 0].reshape((len(positions[:, 0]), 1))
     distance = la.norm(rij, axis=0)  # distance[0] is distance to sun
-    print(np.size(distance))
     a = cnst.G * np.sum(mass * rij / (distance**3), axis=1)
     return a
 
@@ -60,12 +60,15 @@ def driver(f, a, ya, b, yb, h, acc, eps, stepper, limit, max_factor, estimate_en
     is fixed to ya length of 6 (3 cartesian coordinates, 3 cartesian velocities) due to while loop conditions
     """
     nsteps = 0
-    r = np.nan(shape=(6, 1000))
-    t = np.nan(shape=(1000,))
+    r = np.empty(shape=(6, limit), dtype=np.double)
+    t = np.empty(shape=(limit,), dtype=np.double)
+    r[:] = np.NaN
+    t[:] = np.NaN
 
     x = a   # current last step
     yx = ya
     yx0 = ya
+    k = 0
     if estimate_endpos:
         condition = la.norm(yx[0:2] - yb, axis=0) < la.norm(yx0[0:2] - yb, axis=0)
     else:
@@ -86,7 +89,7 @@ def driver(f, a, ya, b, yb, h, acc, eps, stepper, limit, max_factor, estimate_en
         accept = True
         tol_ratio = np.ones(err.size)
         for i in range(0, tau.size):
-            tol_ratio[i] = tau[i]/err[i]
+            tol_ratio[i] = np.abs(tau[i]/err[i])
             if tol_ratio[i] < 1:
                 accept = False
 
@@ -95,15 +98,16 @@ def driver(f, a, ya, b, yb, h, acc, eps, stepper, limit, max_factor, estimate_en
             x += h
             yx0 = yx
             yx = yh
-            t[:, nsteps] = x
-            r[:, nsteps] = yx
+            t[k] = x
+            r[:, k] = yx
+            k += 1
         else:
             print("Bad step at x = ", x)
             print("Step rejected")
         # Adjust stepsize based on empirical formula
         tol_min = tol_ratio[0]
         for i in range(1, tol_ratio.size):
-            tol_min = np.min(tol_min, tol_ratio[i])
+            tol_min = np.min([tol_min, tol_ratio[i]])
         adj_factor = np.power(tol_min, 0.25)*0.95
         if adj_factor > max_factor:
             adj_factor = max_factor
@@ -115,7 +119,8 @@ def driver(f, a, ya, b, yb, h, acc, eps, stepper, limit, max_factor, estimate_en
             condition = la.norm(yx[0:2] - yb, axis=0) < la.norm(yx0[0:2] - yb, axis=0)
         else:
             condition = x < b
-
+    t = np.where(np.isfinite(t), t, 0)
+    r = np.where(np.isfinite(r), r, 0)
     print("ODE solved in ", nsteps, " steps")
     return r, t
 
@@ -142,6 +147,8 @@ def rk45_step(f, t, yt, h):
 
     yh = yt + np.sum(k*b5, axis=1)
     err = yh - yt - np.sum(k*b4, axis=1)
+    print('yh', yh)
+    print('err', err)
     return yh, err
 
 

spacecraft.py
@@ -69,7 +69,7 @@ class SpaceCraft:
         for body in system_bodies.objects:
             if body.name is not 'sun':
                 r_soi = body.sphere_of_influence()
-                if np.abs(body.get_barycentric - self.pos) < r_soi:
+                if la.norm(body.get_barycentric(self.t) - self.pos) < r_soi:
                     within_sphere = np.append(within_sphere, body)
         if len(within_sphere) == 0:
             return system_bodies.get_body('sun')