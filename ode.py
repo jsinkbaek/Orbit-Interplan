first successful test of everything up to hohmann.simple (including ode). temporary fix of time unit to days. Added unitconverter.py to begin more structured handling of units (next commit feature)

ode.py
@@ -14,10 +14,13 @@ def equations(t, r, objects):
                     of current position of all planetoids needed, in solar cartesian coordinates.
     :return: vector dr with dx,dy,dz,ddx,ddy,ddz
     """
+    # print('t', t)
+    # print('r', r)
     dr = np.empty(r.shape)
-    dr[0:3] = r[3:6]
-    obj_pos = objects.ephemeris(t)
+    dr[0:3] = r[3:]
 
+    obj_pos = objects.ephemeris(t)
+    # print('obj_pos', obj_pos)
     positions = np.empty((3, obj_pos[0, :].size+1))
     positions[:, 0] = r[0:3]
     positions[:, 1:] = obj_pos
@@ -33,13 +36,14 @@ def acceleration(positions, mass):
     :param mass: masses of objects. mass[0] is sun, then other objects. numpy 2d array size (1, nobjects)
     :return: returns resulting acceleration a at point r[:, 0]. Should be ndarray with size (3, 1)
     """
-    rij = positions[:, 1:] - positions[:, 0].reshape((len(positions[:, 0]), 1))
+    rij = positions[:, 1:len(positions[0, :])] - positions[:, 0].reshape((len(positions[:, 0]), 1))
     distance = la.norm(rij, axis=0)  # distance[0] is distance to sun
-    a = cnst.G * np.sum(5.9722 * 1e24 * mass * rij / (distance**3), axis=1)  # factor us earth mass to kg
+    a = cnst.G * np.sum(mass * rij / (distance**3), axis=1) * 86400**2  # factor earth mass to kg
     return a
 
 
-def driver(f, a, ya, b, yb, h, acc, eps, stepper, limit, max_factor, estimate_endpos=True, max_stepsize=None):
+def driver(f, a, ya, b, yb, stepper, h=0.1, acc=1e-6, eps=1e-6, limit=1000, max_factor=2, estimate_endpos=True,
+           max_stepsize=None):
     """
     Adaptive ode driver that advances solution by calling rkXX stepper and adjusts to appropriate step sizes.
     Stops at closest approach to a wanted position yb.
@@ -61,7 +65,7 @@ def driver(f, a, ya, b, yb, h, acc, eps, stepper, limit, max_factor, estimate_en
     is fixed to ya length of 6 (3 cartesian coordinates, 3 cartesian velocities) due to while loop conditions
     """
     nsteps = 0
-    r = np.empty(shape=(6, limit), dtype=np.double)
+    r = np.empty(shape=(len(ya), limit), dtype=np.double)
     t = np.empty(shape=(limit,), dtype=np.double)
     r[:] = np.NaN
     t[:] = np.NaN
@@ -90,7 +94,7 @@ def driver(f, a, ya, b, yb, h, acc, eps, stepper, limit, max_factor, estimate_en
         accept = True
         tol_ratio = np.ones(err.size)
         for i in range(0, tau.size):
-            tol_ratio[i] = np.abs(tau[i]/err[i])
+            tol_ratio[i] = np.abs(tau[i])/np.abs(err[i])
             if tol_ratio[i] < 1:
                 accept = False
 
@@ -106,9 +110,10 @@ def driver(f, a, ya, b, yb, h, acc, eps, stepper, limit, max_factor, estimate_en
             print("Bad step at x = ", x)
             print("Step rejected")
         # Adjust stepsize based on empirical formula
-        tol_min = tol_ratio[0]
-        for i in range(1, tol_ratio.size):
-            tol_min = np.min([tol_min, tol_ratio[i]])
+        tol_min = np.min(tol_ratio)
+        # tol_min = tol_ratio[0]
+        # for i in range(1, tol_ratio.size):
+        #     tol_min = np.min([tol_min, tol_ratio[i]])
         adj_factor = np.power(tol_min, 0.25)*0.95
         if adj_factor > max_factor:
             adj_factor = max_factor
@@ -123,8 +128,8 @@ def driver(f, a, ya, b, yb, h, acc, eps, stepper, limit, max_factor, estimate_en
             condition = la.norm(yx[0:2] - yb, axis=0) < la.norm(yx0[0:2] - yb, axis=0)
         else:
             condition = x < b
-    t = np.where(np.isfinite(t), t, 0)
-    r = np.where(np.isfinite(r), r, 0)
+    t = t[~np.isnan(t)]
+    r = r[:, ~np.all(np.isnan(r), axis=0)]
     print("ODE solved in ", nsteps, " steps")
     return r, t
 
@@ -143,7 +148,7 @@ def rk45_step(f, t, yt, h):
     k[:, 0] = h * f(t, yt)
     k[:, 1] = h * f(t+h*1.0/4, yt+k[:, 0]*1.0/4)
     k[:, 2] = h * f(t+h*3.0/8, yt+k[:, 0]*3.0/32+k[:, 1]*9.0/32)
-    k[:, 3] = h * f(t+h*12.0/13, yt+k[:, 0]*1932.0/2197-k[:, 1]*7200.0/2197*+k[:, 2]*7296.0/2197)
+    k[:, 3] = h * f(t+h*12.0/13, yt+k[:, 0]*1932.0/2197-k[:, 1]*7200.0/2197+k[:, 2]*7296.0/2197)
     k[:, 4] = h * f(t+h*1.0, yt+k[:, 0]*439.0/216-k[:, 1]*8.0+k[:, 2]*3680.0/513-k[:, 3]*845.0/4104)
     k[:, 5] = h * f(t+h*0.5, yt-k[:, 0]*8.0/27+k[:, 1]*2.0-k[:, 2]*3544.0/2565+k[:, 3]*1859.0/4104-k[:, 4]*11.0/40)
     b5 = np.array([16.0/135, 0, 6656.0/12825, 28561.0/56430, -9.0/50, 2.0/55])

test.py
@@ -1,51 +1,77 @@
 from spacecraft import SpaceCraft
 from celestials import CelestialBody, CelestialGroup
+from transfer import Hohmann, Rendezvous
 from scipy import constants as cnst
 import numpy as np
 import matplotlib.pyplot as plt
 import ode
 import transfer
-
+m_factor = 5.9722 * 1e24
+a_factor = 1.496 * 1e11
 # Stars
-sun = CelestialBody('sun', 332946.0487, 'star', None, 0.000001)
+sun = CelestialBody('sun', 332946.0487*m_factor, 'star', None, 0.000001*a_factor)
 # Planets
-mercury = CelestialBody('mercury', 0.05527, 'planet', sun, 0.3870993)
-venus = CelestialBody('venus', 0.81500, 'planet', sun, 0.723336)
-earth = CelestialBody('earth', 1, 'planet', sun, 1.000003)
-mars = CelestialBody('mars', 0.10745, 'planet', sun, 1.52371)
-jupiter = CelestialBody('jupiter', 317.83, 'planet', sun, 5.2029)
-saturn = CelestialBody('saturn', 95.159, 'planet', sun, 9.537)
-uranus = CelestialBody('uranus', 14.500	, 'planet', sun, 19.189)
-neptune = CelestialBody('neptune', 17.204, 'planet', sun, 30.0699)
+# mercury = CelestialBody('mercury', 0.05527*m_factor, 'planet', sun, 0.3870993*a_factor)
+# venus = CelestialBody('venus', 0.81500*m_factor, 'planet', sun, 0.723336*a_factor)
+earth = CelestialBody('earth', 1.0*m_factor, 'planet', sun, 1.000003*a_factor)
+# mars = CelestialBody('mars', 0.10745*m_factor, 'planet', sun, 1.52371*a_factor)
+jupiter = CelestialBody('jupiter', 317.83*m_factor, 'planet', sun, 5.2029*a_factor)
+# saturn = CelestialBody('saturn', 95.159*m_factor, 'planet', sun, 9.537*a_factor)
+# uranus = CelestialBody('uranus', 14.500*m_factor, 'planet', sun, 19.189*a_factor)
+# neptune = CelestialBody('neptune', 17.204*m_factor, 'planet', sun, 30.0699*a_factor)
 # Dwarf planets
 # ceres = CelestialBody('ceres', 0.00016, 'planet', sun, 2.7658)
 # Moons
-moon = CelestialBody('moon', 0.0123000371, 'moon', earth, 0.002572)
+# moon = CelestialBody('moon', 0.0123000371*m_factor, 'moon', earth, 0.002572*a_factor)
 
-solar_system = CelestialGroup(sun, mercury, venus, earth, mars, jupiter, saturn, uranus, neptune, moon)
+solar_system = CelestialGroup(sun, earth, jupiter)
+# solar_system = CelestialGroup(sun, mercury, venus, earth, mars, jupiter, saturn, uranus, neptune, moon)
 
-distance = 381000
+distance = 10000 * 1e3
 t_jd = np.double(800)
-v_circ = np.sqrt(cnst.G * earth.mass / distance)
+v_circ = np.sqrt(cnst.G * earth.mass / distance) * 86400
 satellite_pos = earth.get_barycentric(t_jd) + np.array([distance, 0, 0])
 satellite_vel = earth.get_barycentric_vel(t_jd) + np.array([0, v_circ, 0])
 billy = SpaceCraft(satellite_pos, t_jd, satellite_vel, solar_system)
-dt = 3000
+dt = 10
 
+v_esc = np.sqrt(2*cnst.G*earth.mass/distance) * 86400
+dv1 = v_esc - v_circ
+billy.update(billy.pos, billy.t, billy.velocity + np.array([0, dv1, 0]))
+hohmann1 = Hohmann(billy)
+print(hohmann1.simple(body=sun))
 
-plt.plot()
-ts, ys = billy.calculate_trajectory(t_jd+dt, max_stepsize=0.01)
-pos_res = ys[0:3, :]
-pos_rela = pos_res # - earth.get_barycentric(ts)
-moon_rela = moon.get_barycentric(ts) # - earth.get_barycentric(ts)
-# plt.plot(moon_rela[0, :], moon_rela[1, :], 'k.')
-plt.plot(pos_rela[0, :], pos_rela[1, :], 'b*')
+"""
 ts, ys = billy.calculate_trajectory(t_jd+dt)
 pos_res = ys[0:3, :]
 pos_rela = pos_res # - earth.get_barycentric(ts)
-# plt.plot(pos_rela[0, :], pos_rela[1, :], 'r*')
-print(pos_rela[:, 23])
-print(pos_rela[:, 24])
-print(pos_rela[:, 25])
-print(pos_rela[:, 26])
+moon_rela = moon.get_barycentric(ts) # - earth.get_barycentric(ts)
+earth_pos = earth.get_barycentric(ts)
+sun_pos = sun.get_barycentric(ts)
+plt.plot(moon_rela[0, :], moon_rela[1, :], 'k.')
+plt.plot(earth_pos[0, :], earth_pos[1, :], 'b.')
+plt.plot(sun_pos[0, :], sun_pos[1, :], 'y.')
+plt.plot(pos_rela[0, :], pos_rela[1, :], 'r*')
+print(len(np.where(pos_rela == 0)[0]))
+print(pos_rela.shape)
 plt.show()
+"""
+
+"""
+def eqsin(x, y):
+    return np.array([y[1], -y[0]])
+
+
+a = 0
+ya = np.array([0, 1])
+b = 3*np.pi
+yb = 1
+
+r_, t_ = ode.driver(eqsin, a, ya, b, yb=yb, stepper=ode.rk45_step, estimate_endpos=False)
+
+t_sin = np.linspace(0, 3*np.pi)
+plt.plot(t_sin, np.sin(t_sin))
+plt.plot(t_, r_[0, :], '*')
+print(r_.shape)
+plt.show()
+"""

transfer.py
@@ -16,8 +16,8 @@ class Hohmann:
         if body is None:
             body = self.spacecraft.get_current_body(self.spacecraft.system_bodies)
         if r2 is None:
-            la.norm(body.get_barycentric(self.spacecraft.t), axis=0)
-        dv1 = np.sqrt(cnst.G * body.mass / r1) * (np.sqrt(2*r2/(r1+r2)) - 1)
+            r2 = la.norm(body.get_barycentric(self.spacecraft.t), axis=0)
+        dv1 = np.sqrt(cnst.G * body.mass / r1) * (np.sqrt(2.*r2/(r1+r2)) - 1)
         dv2 = np.sqrt(cnst.G * body.mass / r2) * (1 - np.sqrt(2*r1/(r1+r2)))
         dv = dv1 + dv2
         tH = np.pi * np.sqrt((r1+r2)**3 / (8*cnst.G*body.mass))